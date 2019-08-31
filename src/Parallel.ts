4349abbe473df84cef9bba2e0ed62ba028276c11
Piotr <37638629+tanfonto@users.noreply.github.com>
2019-08-31 16:06:34 +0200

    Update Parallel.ts

--- src/Parallel.ts
+++ src/Parallel.ts
@@ -2,8 +2,8 @@ import { AsyncTask, ParallelTask, Variant } from '../@types';
 import { lift } from './utils/array';
 
 export const Parallel = <a>(
-  ...tasks: IO<AsyncTask<a>>[]
-): IParallelTask<a> => () => {
+  ...tasks: AsyncTask<a>[]
+): ParallelTask<a> => () => {
   const results = Array.of<Variant<a>>();
   return new Promise(f => {
     tasks.forEach(async task => {
