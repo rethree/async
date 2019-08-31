f7631e2db6ad8b8d11035b9055c68973ea316be8
Piotr <37638629+tanfonto@users.noreply.github.com>
2019-08-31 13:14:20 +0200

    Update and rename adt.d.ts to adt.ts

---
+++ @types/adt.ts
@@ -0,0 +1,17 @@
+type _ = unknown;
+
+export type Morphism<a, b> = (x: a) => b;
+
+export type EndoMorphism<a> = Morphism<a, a>;
+
+export type EndoFunctor<a, w> = {
+  map: <Fa extends EndoFunctor<a, w> & w>(m: EndoMorphism<a>) => Fa;
+};
+
+export type FAlgebra<a, w = _> = EndoFunctor<a, w> & w & { alg: () => a };
+
+export type InitialAlgebra<
+  a,
+  w = _,
+  Fa extends FAlgebra<a, w> = FAlgebra<a, w>
+> = [(x: a) => Fa, (x: a, fa: Fa) => Fa, () => Fa];
