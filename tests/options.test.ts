af2d53ad13a040cc22c31c57e3ff1c2413cec48d
tanfonto <tanfonto@pm.me>
2019-09-13 21:37:04 +0200

    missing coverage

--- tests/options.test.ts
+++ tests/options.test.ts
@@ -1,7 +1,27 @@
-// import { test } from 'tap';
+import { test } from 'tap';
+import { Faulted, isFaulted, Completed, allCompleted } from '../src';
+import { Option } from '../@types';
 
-// test('isFaulted detects faulted variant', async t => {
-//   const fault = Faulted(Error('42'));
+test('isFaulted detects faulted variant', async t => {
+  const fault = Faulted(Error('42'));
 
-//   t.true(isFaulted(fault[0]));
-// });
+  t.true(isFaulted(fault[0]));
+});
+
+test('allCompleted detects failures withing mixed results', async t => {
+  const xs = [...Faulted(Error('42')), ...Completed(42)];
+
+  t.false(allCompleted(xs));
+});
+
+test('allCompleted detects failures withing failed results', async t => {
+  const xs = [...Faulted(Error('42')), ...Faulted(42)];
+
+  t.false(allCompleted(xs));
+});
+
+test('allCompleted detects complete-only results', async t => {
+  const xs: Option<any>[] = [...Completed('9001'), ...Completed(42)];
+
+  t.true(allCompleted(xs));
+});
