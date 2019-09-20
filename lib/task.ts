2fea029bd903182cb962ed06a7c7ba580cb8c63b
tanfonto <tanfonto@pm.me>
2019-09-20 10:08:20 +0200

    better rejection handling

--- lib/task.ts
+++ lib/task.ts
@@ -1,13 +1,14 @@
-import { AsyncTask, Options } from './types';
 import { Option } from './options';
+import { AsyncTask, Options } from './types';
 
 export const Task = <a, bs extends any[]>(
   x: Promise<a> | ((...args: bs) => Promise<a>)
 ): AsyncTask<a> => (...args: bs) => {
   const thenable = x instanceof Promise ? x : x(...args);
-  return thenable
-    .then(x => [Option<a>().Completed({ value: x as a, meta: { args } })])
-    .catch(x => [Option<a>().Faulted({ fault: x as a, meta: { args } })]);
+  return thenable.then(
+    value => [Option<a>().Completed({ value, meta: { args } })],
+    fault => [Option<a>().Faulted({ fault, meta: { args } })]
+  );
 };
 
 export const complete = <a>(x: a | Promise<a>): AsyncTask<a> =>
